fc23c31476886f9cca22f14f514f01e76dec55e0
dadpem <mandardadpe@yahoo.com>
2015-12-11 16:20:07 +0530

    Added comments for each class, trait and obejcts
    Project : ScalaLab
    Author : Mandar Dadpe

---
+++ src/main/scala/com/ibm/scalalab/api/AccountSummaryInvoker.scala
@@ -0,0 +1,35 @@
+package com.ibm.scalalab.api
+
+/** imports scalaxb generated package */
+import generated._
+
+import scala.concurrent.Future
+import scalaxb.{DispatchHttpClientsAsync, Soap11ClientsAsync}
+
+/**
+ * AccountSummaryInvoker object hits SOAP webservice using scalaxb to get the response and writes it to reqired response format.
+ * Also, describes 'Future' which is an object holding value which may or may not exists.
+ */
+object AccountSummaryInvoker {
+
+  def getAccountSummary : String = {
+    /** this is scalaxb's async implementation of SOAP envelope case classes */
+    val remote = new AccountSummarySoapBindings with Soap11ClientsAsync with DispatchHttpClientsAsync{}
+    var accountStr:String = "Hello"
+
+    /** future method starts asynchronous computation of account summary and returns a future holding response of customer's account details */
+    val response:Future[Account] = remote.service.getAccountSummary(Some("mandadpe@in.ibm.com"),Some("Mandar"),Some("Dadpe"),Some("9420411799"))
+
+    /** writes SOAP response to JSON format and return as a string  */
+    response.onComplete(account=>{
+      accountStr = AccountSummaryJsonProtocol.AccountSummaryResponseFormat.write(AccountSummaryResponse(account.get.accountId.get,account.get.accountType.get,account.get.balance.get)).toString()
+      println(accountStr)
+    })
+
+    while(!response.isCompleted){
+      Thread.sleep(1000)
+    }
+    accountStr
+  }
+
+}
